de8ef515dfceba876c6475d1e46b4b335c0cc908
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2020-06-11 16:54:32 +0200

    Initial project

---
+++ Server/Controllers/QuizController.cs
@@ -0,0 +1,38 @@
+﻿using QuizManagerClientHosted.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace QuizManagerClientHosted.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuizController : ControllerBase
+    {
+        private static readonly List<QuizItem> Quiz = new List<QuizItem> {
+            new QuizItem
+                {
+                    Question = "Which of the following is the name of a Leonardo da Vinci's masterpiece?",
+                    Choices = new List<string> {"Sunflowers", "Mona Lisa", "The Kiss"},
+                    AnswerIndex = 1,
+                    Score = 3
+                },
+                new QuizItem
+                {
+                    Question = "Which of the following novels was written by Miguel de Cervantes?",
+                    Choices = new List<string> {"The Ingenious Gentleman Don Quixote of La Mancia", "The Life of Gargantua and of Pantagruel", "One Hundred Years of Solitude"},
+                    AnswerIndex = 0,
+                    Score = 5
+                }
+            };
+
+        [HttpGet]
+        public List<QuizItem> Get()
+        {
+            return Quiz;
+        }
+    }
+}
---
+++ Shared/QuizItem.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace QuizManagerClientHosted.Shared
+{
+    public class QuizItem
+    {
+        public string Question { get; set; }
+        public List<string> Choices { get; set; }
+        public int AnswerIndex { get; set; }
+        public int Score { get; set; }
+
+        public QuizItem()
+        {
+            Choices = new List<string>();
+        }
+    }
+}
